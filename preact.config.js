dec67ef8e22bcafadd3a364ab902329393976031
Hassan Bazzi <contact@hassanbazzi.com>
2019-05-13 03:37:30 +0200

    fix robots

--- preact.config.js
+++ preact.config.js
@@ -1,17 +1,20 @@
-import { resolve } from 'path'
-const netlifyPlugin = require('preact-cli-plugin-netlify');
-import Critters from 'critters-webpack-plugin';
+import { resolve } from "path";
+const netlifyPlugin = require("preact-cli-plugin-netlify");
+import Critters from "critters-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 
-export default function (config, env, helpers) {
-  config.resolve.alias.src = resolve(__dirname, './src')
-  config.resolve.alias.components = resolve(__dirname, './src/components')
-  config.resolve.alias.sections = resolve(__dirname, './src/sections')
-  config.resolve.alias.style = resolve(__dirname, './src/style')
-  config.resolve.alias.lib = resolve(__dirname, './src/lib')
+export default function(config, env, helpers) {
+  config.resolve.alias.src = resolve(__dirname, "./src");
+  config.resolve.alias.components = resolve(__dirname, "./src/components");
+  config.resolve.alias.sections = resolve(__dirname, "./src/sections");
+  config.resolve.alias.style = resolve(__dirname, "./src/style");
+  config.resolve.alias.lib = resolve(__dirname, "./src/lib");
 
-  let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
+  let { rule } = helpers.getLoadersByName(config, "babel-loader")[0];
   let babelConfig = rule.options;
-  
-  babelConfig.plugins.push('@babel/plugin-proposal-export-default-from');
-  return netlifyPlugin(config)
+
+  config.plugins.push(new CopyPlugin([{ from: "./robots.txt", to: "./" }]));
+
+  babelConfig.plugins.push("@babel/plugin-proposal-export-default-from");
+  return netlifyPlugin(config);
 }
